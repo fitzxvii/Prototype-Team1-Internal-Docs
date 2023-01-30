0217a5c3b3794235cb532c9509eb461701ab58ed
fitzxvii <fitz.villegas.17@gmail.com>
2023-01-30 08:44:58 +0800

    inital codes for dropdown autocompletre

--- web-frontend/assets/js/custom/invite_docs_modal.js
+++ web-frontend/assets/js/custom/invite_docs_modal.js
@@ -8,6 +8,63 @@ document.addEventListener("DOMContentLoaded", () => {
         limitEmailsToDomain: 'village88',
         validEmailClass: 'valid-email',
     });
+
+    const input = document.querySelector('.email-input');
+    const suggestions = document.querySelector('.suggestions ul');
+    const fruit = ['fvillegas@village88.com', 'jdeleon@village88.com', 'snudo@village88.com', 'ruelito-ytac@village88.com'];
+
+    function search(str) {
+        let results = [];
+        const val = str.toLowerCase();
+
+        for (i = 0; i < fruit.length; i++) {
+            if (fruit[i].toLowerCase().indexOf(val) > -1) {
+                results.push(fruit[i]);
+            }
+        }
+
+        return results;
+    }
+
+    function searchHandler(e) {
+        const inputVal = e.currentTarget.value;
+        let results = [];
+        if (inputVal.length > 0) {
+            results = search(inputVal);
+        }
+        showSuggestions(results, inputVal);
+    }
+
+    function useSuggestion(e) {
+        input.value = e.target.innerText;
+        input.focus();
+        suggestions.innerHTML = '';
+        suggestions.classList.remove('has-suggestions');
+    }
+
+    function showSuggestions(results, inputVal) {
+        suggestions.innerHTML = '';
+
+        if (results.length > 0) {
+            for (i = 0; i < results.length; i++) {
+                let item = results[i];
+                // Highlights only the first match
+                // TODO: highlight all matches
+                const match = item.match(new RegExp(inputVal, 'i'));
+                item = item.replace(match[0], `<strong>${match[0]}</strong>`);
+                suggestions.innerHTML += `<li>${item}</li>`;
+            }
+            suggestions.classList.add('has-suggestions');
+        } else {
+            results = [];
+            suggestions.innerHTML = '';
+            suggestions.classList.remove('has-suggestions');
+        }
+    }
+
+    input.addEventListener('keyup', searchHandler);
+    suggestions.addEventListener('click', useSuggestion);
+
 });
 
 const showInviteDocumentModal = () => {
@@ -179,3 +236,4 @@ function EmailsInput(selector, options) {
         },
     };
 }
+
